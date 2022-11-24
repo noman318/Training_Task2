6ccd1a2cd1bec1293455be1d0b3f7377b7e0a7e6
noman318 <shaikhmohdnoman612@gmail.com>
2022-11-24 18:49:03 +0530

    CSS added in all files

--- index.js
+++ index.js
@@ -11,42 +11,75 @@ const server = http.createServer((req, res) => {
     res.end();
   } else if (req.url == "/createfile") {
     if (fs.existsSync("crud.txt")) {
-      res.end("File is already exists");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`<a href="/" style="padding: 15px 30px;background-color: #5d70c3;border-radius:10px; color: white; font-size:1.5em; text-decoration:none; margin-left: 60px;">Home</a> <br >
+      <h3 style="color: red">File is already exists</h3>`);
+      res.end();
     } else {
-      fs.writeFile("crud.txt", "Welcome to the neosoft 1 file", (err) => {
-        if (err) throw err;
-        else {
-          res.end("File is created");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      fs.writeFile(
+        "crud.txt",
+        "Welcome to the My HTTP crud operations file",
+        (err) => {
+          if (err) throw err;
+          else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(`<br ><a href="/" style="margin-top:10px; padding: 15px 30px;background-color: #5d70c3;border-radius:10px; color: white; font-size:1.5em; text-decoration:none; margin-left: 60px;">Home</a> <br >
+          <h3 style="color: green">File is created</h3>`);
+            res.end();
+          }
         }
-      });
+      );
     }
   } else if (req.url == "/readfile") {
     if (fs.existsSync("crud.txt")) {
       let data = fs.readFileSync("crud.txt");
-      res.end(data.toString());
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`<br ><a href="/" style="margin-top:10px; padding: 15px 30px;background-color: #5d70c3;border-radius:10px; color: white; font-size:1.5em; text-decoration:none; margin-left: 60px;">Home</a> <br >
+      <h3 style="color: green">${data.toString()}</h3>`);
+      res.end();
     } else {
-      res.end("File is not readed please check");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`<br ><a href="/" style="margin-top:10px; padding: 15px 30px;background-color: #5d70c3;border-radius:10px; color: white; font-size:1.5em; text-decoration:none; margin-left: 60px;">Home</a> <br >
+      <h3 style="color: red">File cannot be readed please check</h3>`);
+      res.end();
     }
   } else if (req.url == "/updatefile") {
     if (fs.existsSync("crud.txt")) {
       fs.appendFileSync("crud.txt", "This is apppended line this is new line");
-      res.end("file is updated");
+      // res.writeHead(200, { "Content-Type": "text/html" });
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`<br ><a href="/" style="margin-top:10px; padding: 15px 30px;background-color: #5d70c3;border-radius:10px; color: white; font-size:1.5em; text-decoration:none; margin-left: 60px;">Home</a> <br >
+      <h3 style="color: green">File is updated</h3>`);
+      res.end();
     } else {
-      res.end("File is no updated");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`<br ><a href="/" style="margin-top:10px; padding: 15px 30px;background-color: #5d70c3;border-radius:10px; color: white; font-size:1.5em; text-decoration:none; margin-left: 60px;">Home</a> <br >
+      <h3 style="color: red">File could not be updated</h3>`);
     }
   } else if (req.url == "/deletefile") {
     if (fs.existsSync("crud.txt")) {
       fs.unlink("crud.txt", (err) => {
         if (err) throw err;
         else {
-          res.end("File is deleted");
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(`<br ><a href="/" style="margin-top:10px; padding: 15px 30px;background-color: #5d70c3;border-radius:10px; color: white; font-size:1.5em; text-decoration:none; margin-left: 60px;">Home</a> <br >
+          <h3 style="color: green">File is deleted</h3>`);
+
+          res.end();
         }
       });
     } else {
-      res.end("File is not exist");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`<br ><a href="/" style="margin-top:10px; padding: 15px 30px;background-color: #5d70c3;border-radius:10px; color: white; font-size:1.5em; text-decoration:none; margin-left: 60px;">Home</a> <br >
+      <h3 style="color: red">File does not exists</h3>`);
+      res.end();
     }
   } else {
-    res.end("Enter Valid url");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(`<br ><a href="/" style="margin-top:10px; padding: 15px 30px;background-color: #5d70c3;border-radius:10px; color: white; font-size:1.5em; text-decoration:none; margin-left: 60px;">Home</a> <br >
+    <h3 style="color: red">Invalid URL</h3>`);
+    res.end();
   }
 });
 server.listen(PORT, (err) => {
